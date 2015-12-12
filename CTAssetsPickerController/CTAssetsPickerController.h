46ca35678fc6b97b3a3043dd72c8e3653f05b750
aureliensibiril <aurelien@ideanov.com>
2015-12-12 11:01:51 +0100

    f

--- CTAssetsPickerController/CTAssetsPickerController.h
+++ CTAssetsPickerController/CTAssetsPickerController.h
@@ -140,7 +140,7 @@
 @property (nonatomic, readonly, strong) UISplitViewController *childSplitViewController;
 
 @property (nonatomic, assign) BOOL fromPopupTimeline;
-@property (weak, nonatomic) id *delegateTimeline;
+@property (weak, nonatomic) id delegateTimeline;
 /**
  *  @name Managing Selections
  */
